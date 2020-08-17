50a96d4c289db8d0da4e9297427df27fa9dee2fc
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-18 00:49:28 +0200

    link layout elements to code

--- app/src/main/java/hr/ficko/dicee/MainActivity.java
+++ app/src/main/java/hr/ficko/dicee/MainActivity.java
@@ -2,6 +2,8 @@ package hr.ficko.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton = (Button) findViewById(R.id.rollButton);
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
     }
 }
