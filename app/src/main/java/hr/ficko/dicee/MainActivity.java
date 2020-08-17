d3ca2b119f1180264e692ead4ad9248e3b1c341f
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-18 01:00:53 +0200

    add random number generator feature

--- app/src/main/java/hr/ficko/dicee/MainActivity.java
+++ app/src/main/java/hr/ficko/dicee/MainActivity.java
@@ -2,8 +2,12 @@ package hr.ficko.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         Button rollButton = (Button) findViewById(R.id.rollButton);
         ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
         ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6) + 1;
+                Log.d("Dicee", "The random number is " + number);
+            }
+        });
     }
 }
